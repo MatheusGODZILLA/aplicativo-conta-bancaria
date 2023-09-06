26ee83fc9e478305f58afd5154b48740915970e0
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-09-06 01:57:35 -0300

    Declarando os atributos necessários

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+    String nome;
+    double saldo;
+    String email;
+
+    public Cliente(String nome, double saldo, String email) {
+        this.nome = nome;
+        this.saldo = saldo;
+        this.email = email;
+    }
+}
