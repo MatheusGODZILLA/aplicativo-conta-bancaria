2c939f3705d26bd4a01968f1a0c500bce2640e29
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-09-06 04:13:06 -0300

    Criando a classe Conta

--- Banco.ts
+++ Banco.ts
@@ -11,3 +11,17 @@ class Cliente {
       this.email = email;
     }
 }
+
+// Classe da Conta
+class Conta {
+    saldo: number;
+    numeroConta: number;
+    cliente: Cliente;
+    private historicoTransacoes: string[];
+  
+    constructor(saldo: number, numeroConta: number, cliente: Cliente) {
+      this.saldo = saldo;
+      this.numeroConta = numeroConta;
+      this.cliente = cliente;
+      this.historicoTransacoes = [];
+    }
