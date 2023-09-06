6aaf1aa6a3e572987cc97a66b2e429c35b535c68
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-09-06 02:19:27 -0300

    Declarando métodos: saque, transferência, depósito

--- Conta.java
+++ Conta.java
@@ -16,4 +16,44 @@ public class Conta {
         this.cliente = cliente;
         this.historicoTransacoes = new ArrayList<>();
     }
+
+    // Método para realizar saques
+    public void saque(double valor) {
+        if (valor > saldo) {
+            System.out.println("Saldo insuficiente para o saque.");
+        } else {
+            saldo -= valor;
+            String descricaoTransacao = "Saque: R$" + valor;
+            registrarTransacao(descricaoTransacao);
+            System.out.println("Saque de " + valor + " realizado com sucesso.");
+            System.out.println("\n");
+        }
+    }
+    
+    // Método para realizar depósitos
+    public void deposito(double valor) {
+        saldo += valor;
+        String descricaoTransacao = "Depósito: R$" + valor;
+        registrarTransacao(descricaoTransacao);
+        System.out.println("Depósito de " + valor + " realizado com sucesso.");
+        System.out.println("\n");
+    }    
+
+    // Método para realizar transferências
+    public void transferencia(Conta destino, double transferencia) {
+        if (transferencia > saldo) {
+            System.out.println("Saldo insuficiente para a transferência.");
+            System.out.println("\n");
+        } else {
+            saldo -= transferencia;
+            destino.deposito(transferencia);
+    
+            // Registra a transação no histórico
+            String descricaoTransacao = "Transferência para a conta " + destino.numeroConta + ": R$" + transferencia;
+            registrarTransacao(descricaoTransacao);
+    
+            System.out.println("Transferência de " + transferencia + " realizada com sucesso para o destinatário.");
+            System.out.println("\n");
+        }
+    }
 }
