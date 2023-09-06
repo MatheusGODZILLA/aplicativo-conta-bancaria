511236846d8d29fb8fa84de0f7d1810b2c963eb4
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-09-06 02:15:37 -0300

    Declarando importações, atributos e o construtor

--- Conta.java
+++ Conta.java
@@ -1,3 +1,19 @@
+// Importando estruturas para criar e manipular listas
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conta {
-    
+    double saldo;
+    int numeroConta;
+    Cliente cliente;
+
+    private List<String> historicoTransacoes; // Lista para armazenar o histórico de transações
+
+    // Construtor da classe
+    public Conta(double saldo, int numeroConta, Cliente cliente) {
+        this.saldo = saldo;
+        this.numeroConta = numeroConta;
+        this.cliente = cliente;
+        this.historicoTransacoes = new ArrayList<>();
+    }
 }
