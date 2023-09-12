aaaa5aac54bd76486ddb668ce6f19e08a530a939
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-09-12 01:27:27 -0300

    Aprimorando o cliente com outras informações

--- Cliente.java
+++ Cliente.java
@@ -1,10 +1,14 @@
 public class Cliente {
     String nome;
+    String CPF;
+    String dataNascimento;
     double saldo;
     String email;
 
-    public Cliente(String nome, double saldo, String email) {
+    public Cliente(String nome, String CPF, String dataNascimento, double saldo, String email) {
         this.nome = nome;
+        this.CPF = CPF;
+        this.dataNascimento = dataNascimento;
         this.saldo = saldo;
         this.email = email;
     }
